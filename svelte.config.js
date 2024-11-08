c788fcbc89f3838d1aaf824b9ae2c5e88e4f35a2
YeshwanthGanji <yeshwanth.ganji@tesseractesports.com>
2024-11-08 13:20:31 +0530

    updated svelteconfig.js

---
+++ svelte.config.js
@@ -0,0 +1,33 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+ 
+export default {
+  preprocess: svelte(),
+ 
+  kit: {
+    // Adapter is not needed if you’re only building a component library.
+  },
+ 
+  vite: defineConfig({
+    plugins: [svelte()],
+ 
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'index.js'), // Entry file to export components
+        name: 'KieComponentLibrary',
+        formats: ['es', 'umd'],
+        fileName: (format) => `kie-component-library.${format}.js`,
+      },
+      rollupOptions: {
+        // Externalize dependencies to keep bundle size small
+        external: ['svelte', 'svelte/internal'],
+        output: {
+          globals: {
+            svelte: 'Svelte',
+          },
+        },
+      },
+    },
+  }),
+};
